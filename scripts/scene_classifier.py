b5c14c51a4b4837777394ca3c79c376b77e2b8c4
John Duncan <john.a.duncan@utexas.edu>
2023-09-28 16:57:32 -0500

    Add transformer model for scene classification

--- scripts/scene_classifier.py
+++ scripts/scene_classifier.py
@@ -1,17 +1,16 @@
 import os
-from joblib import load
 
 import rospy
 import rospkg
-from hri_msgs.msg import AudioFeatures
+
+from audio_common_msgs import AudioDataStamped
 from std_msgs.msg import String
 
 class SceneClassifier:
 
     def __init__(self):
         # Set up ROS node, publishers, subscribers
-        rospy.init_node('scene_classifier_node')
-        self.feature_data_sub = rospy.Subscriber('/audio/audio_features', AudioFeatures, self.feature_data_callback)
+        self.feature_data_sub = rospy.Subscriber('/audio/audio_stamped', AudioDataStamped, self.audio_data_callback)
         self.scene_class_pub = rospy.Publisher('scene_class', String, queue_size=10)
 
         # Initialize member variables
@@ -19,30 +18,14 @@ class SceneClassifier:
         self.pkg_path = self.rospack.get_path('hri_audition')
         self.model_path = self.pkg_path + '/data/models/'
 
-        # Load classifier models
-        # self.clfs = load_classifiers()
-
-    # def load_classifiers(self):
-    #     # Search through model folder
-    #     for root, dirs, files in os.walk(self.feat_path,topdown=True):
-
-
-    #     # Add each model to list of classifiers and return
-
-    # def predict(self,feature_msg):
-    #     # Convert feature message into vector
-    #     # Predict class of feature vector
-    #     # Return result
-
-    def feature_data_callback(self, feature_msg):
-        # Do something with the audio data message
-        # Process the audio data and generate a string output
-        output_data = "Feature data received"
+    def audio_data_callback(self, feature_msg):
+        output_data = "Audio data received"
         self.scene_class_pub.publish(output_data)
 
     def run(self):
         rospy.spin()
 
 if __name__ == '__main__':
+    rospy.init_node('scene_classifier_node')
     node = SceneClassifier()
     node.run()
