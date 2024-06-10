43740fade0d17d0926ebdfa8e45eef02c9ad952e
John Duncan <john.a.duncan@utexas.edu>
2024-06-09 21:23:37 -0500

    Add audio acquisition node

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-#ros_audition
+# ros_audition
 A repo for audition and auditory analysis in ROS2, used for my Ph.D. research in human-robot interaction and human-robot teaming.
 
 A lot of this will be application-specific, but if you have specific questions or would like to collaborate, feel free to reach out to me.
+
+# Setup
+## Finding audio device parameters
+To find your audio input device parameters, install pulseaudio and run:
+```
+pacmd list-sources
+```
+Find your device's index and note the following lines:
+```
+sample spec: s16le 1ch 48000Hz
+device.string = "hw:3"
+device.description = "Webcam C270 Mono"
+```
+
+Then, create a `<device>_config.yaml file`, using the provided examples as a starting point.
+
+# Future work
+## Improvements
+- Refactor launch file input arguments
+- Account for 16/32 signed/unsigned datatypes in audio_acq's audio frame
+- Handle the audio_common_msgs dependency and build error
---
+++ launch/audio_acq.launch.py
@@ -0,0 +1,34 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch import LaunchDescription
+from launch_ros.actions import Node, LoadComposableNodes, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Config files
+    audio_config = os.path.join(
+        get_package_share_directory('ros_audition'),
+        'config',
+        'usb_config.yaml'
+    )
+
+    # Audio acquisition node
+    acq_node = Node(
+        package='ros_audition',
+        executable='audio_acq_node.py',
+        name='audio_acq_node',
+        output='screen',
+        parameters=[audio_config]
+    )
+    ld.add_action(acq_node)
+
+    return ld
